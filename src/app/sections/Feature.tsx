02063ad61fba6918229a40727baa65cc471d5268
CodingMileage <72987158+CodingMileage@users.noreply.github.com>
2024-09-14 10:46:59 -0500

    UI

--- src/app/sections/Feature.tsx
+++ src/app/sections/Feature.tsx
@@ -83,7 +83,7 @@ export const TextParallaxContentExample = () => {
       </TextParallaxContent>
 
       <TextParallaxContent
-        imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        imgUrl="https://images.unsplash.com/photo-1697577418970-95d99b5a55cf?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Those were just the tip of the iceberg"
         heading="Solve more of your issues instantly with FixChat"
       />
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -95,7 +95,7 @@ export const InfiniteMovingCards = ({
             className="w-[350px] max-w-full relative rounded-2xl px-8 py-6 md:w-[450px]"
             style={{
               background:
-                "linear-gradient(180deg, var(--purple-800), var(--slate-900)",
+                "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
             }}
             key={item.name}
           >
@@ -105,15 +105,15 @@ export const InfiniteMovingCards = ({
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
               <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
-                {item.quote}
+                {item.name}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                   <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.name}
+                    {item.title}
                   </span>
                   <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
-                    {item.title}
+                    {item.quote}
                   </span>
                 </span>
               </div>
